028beabca9ae4a1d1e8c422dea68b17da9e13cec
devinaanderson <daa7896@gmail.com>
2019-01-11 05:54:11 -0700

    first commit

---
+++ ecommerce/views.py
@@ -0,0 +1,113 @@
+from django.contrib.auth import authenticate, login, get_user_model
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm, LoginForm, RegisterForm
+
+def home_page(request):
+    context = {
+        "title":"Hello World!!",
+        "content":"Welcome to the home page!"
+    }
+    if request.user.is_authenticated():
+        context["premium_content"]="Now that you are logged in, you have full access. Enjoy!"
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title":"Hello World!!",
+        "content":"Welcome to the about page!"
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title":"Hello World!!",
+        "content":"Welcome to the contact page!",
+        "form": contact_form
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    if request.method == 'POST':
+        # print(request.POST)
+        print(request.POST.get('fullname'))
+        print(request.POST.get('email'))
+        print(request.POST.get('content'))
+
+    return render(request, "contact/view.html", context)
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    print("User logged in")
+    print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(request.user.is_authenticated())
+
+        if user is not None:
+            print(request.user.is_authenticated())
+            login(request, user)
+            # redirect to success page
+            # context['form'] = LoginForm()
+            return redirect("/login")
+        else:
+            # return an 'invalid login' error message
+            print("error :(")
+    
+    return render(request, "auth/login.html", context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        email = form.cleaned_data.get("email")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+    return render(request, "auth/register.html", context)
+
+
+def home_page_old(request):
+    html_ = """
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Bootstrap 101 Template</title>
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css" integrity="sha384-PmY9l28YgO4JwMKbTvgaS7XNZJ30MK9FAZjjzXtlqyZCqBY6X6bXIkM++IkyinN+" crossorigin="anonymous">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <h1>Hello, world!</h1>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js" integrity="sha384-vhJnz1OVIdLktyixHY4Uk3OHEwdQqPppqYR8+5mjsauETgLOcEynD9oPHhhz18Nw" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    """
+    return HttpResponse(html_)
